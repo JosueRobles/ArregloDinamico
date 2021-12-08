add847cbaa5ba7424fd761ad7467c1b1c6b16ec1
JosueRobles <95750114+JosueRobles@users.noreply.github.com>
2021-12-08 16:50:35 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,18 +4,19 @@ using namespace std;
 int main()
 {
     ArregloDinamico arreglodinamico;
+    arreglodinamico.push_back("Hermana");
     arreglodinamico.push_back("Mama");
+    arreglodinamico.push_front("Hermano");
     arreglodinamico.push_front("Papa");
-    arreglodinamico.insert("Bebe",1);
-    cout << endl;
+    arreglodinamico.insert("Bebe",2);
     cout << "numero de elementos: " << arreglodinamico.size() << endl;
     for(size_t i = 0; i < arreglodinamico.size(); i++)
     {
-        cout << arreglodinamico[i] << " ";
+        cout << arreglodinamico[i] << endl;
     }
     arreglodinamico.pop_back();
     arreglodinamico.pop_front();
-    arreglodinamico.erase(3);
+    arreglodinamico.erase(0);
     cout << "numero de elementos: " << arreglodinamico.size() << endl;
     for(size_t i = 0; i < arreglodinamico.size(); i++)
     {
